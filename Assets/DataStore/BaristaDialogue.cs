16f31bc5a2eff5d7e832979d55c7bf462cfc7f68
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-07-08 14:47:59 +1000

    Implement the GameDataStore

---
+++ Assets/DataStore/BaristaDialogue.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaristaDialogue", menuName = "Barista Dialogue")]
+public class BaristaDialogue : ScriptableObject
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    [Header("Barista Dialogue")]
+    [Space]
+    public List<DialogueObject> dialogueObjects;
+
+    #endregion Fields
+
+}
+
+
+[System.Serializable]
+public class DialogueObject
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    /// <summary>
+    /// The unique identifier for the dialogue object.
+    /// </summary>
+    public string identifier;
+
+    /// <summary>
+    /// The question literal for the dialogue.
+    /// </summary>
+    public string question;
+
+    /// <summary>
+    /// The optional response literals for additional dialogue.
+    /// </summary>
+    public List<string> response;
+
+    #endregion Fields
+
+}
+
---
+++ Assets/DataStore/GameDataStore.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public interface IGameDataStore
+{
+
+    #region - - - - - - Methods - - - - - -
+
+    DialogueObject GetDialogueObjectByIdentifier(string indentifier);
+    string GetDialogueQuestionByIdentifier(string indentifier);
+    List<string> GetDialogueResponsesByIdentifier(string indentifier);
+
+    #endregion Methods
+
+}
+
+public class GameDataStore : MonoBehaviour, IGameDataStore
+{
+
+    #region - - - - - - Fields - - - - - -
+
+    private BaristaDialogue baristaDialogueScriptableObject;
+
+    #endregion Fields
+
+    #region - - - - - - Methods - - - - - -
+
+    DialogueObject IGameDataStore.GetDialogueObjectByIdentifier(string indentifier)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    string IGameDataStore.GetDialogueQuestionByIdentifier(string indentifier)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    List<string> IGameDataStore.GetDialogueResponsesByIdentifier(string indentifier)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #endregion Methods
+
+}
