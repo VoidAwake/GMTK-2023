23e5da200a461bf3c45709ffef71592fb24954ec
johnc1999@hotmail.com <50039786+JohnCollns@users.noreply.github.com>
2023-07-09 00:39:02 +1000

    Added colours to ECG

--- Assets/Scripts/EcgAnimation.cs
+++ Assets/Scripts/EcgAnimation.cs
@@ -6,11 +6,15 @@ public class EcgAnimation : MonoBehaviour
 {
     //private TrailRenderer trailRenderer;
     private Transform activeTrailObject;
-    private Transform[] trailObjects;
-    private TrailRenderer[] trailRenderers;
+    private List<Transform> trailObjects = new List<Transform>();
+    private List<TrailRenderer> trailRenderers = new List<TrailRenderer>();
     private int currentTrailObject = 0;
 
-    public float currentPanic;
+    /// <summary>
+    /// Please set this [0,1] for the current stress of the player. 0 indicates no stress, and 1 is game ending catastrophic stress. 
+    /// </summary>
+    [Range(0,1)] public float currentPanic;
+    private float prevPanic = 0f;
     public bool active = true;
 
     [SerializeField] private float amplitudeResting;
@@ -19,6 +23,8 @@ public class EcgAnimation : MonoBehaviour
     [SerializeField] private float coordSpeedPanic;
     [SerializeField] private float horizSpeedResting;
     [SerializeField] private float horizSpeedPanic;
+    [SerializeField] private float trailTimeResting;
+    [SerializeField] private float trailTimePanic;
     [SerializeField] private float graphWidth;
 
     [SerializeField] private Gradient colorGradient;
@@ -29,7 +35,22 @@ public class EcgAnimation : MonoBehaviour
     private float coordInterpolation = 0f;
     void Start()
     {
-        activeTrailObject = trailObjects[currentTrailObject];
+        for (int i=0; i < transform.childCount; i++)
+        {
+            try
+            {
+                TrailRenderer newTrailRenderer = transform.GetChild(i).GetComponent<TrailRenderer>();
+                if (newTrailRenderer != null)
+                {
+                    trailRenderers.Add(newTrailRenderer);
+                    trailObjects.Add(transform.GetChild(i));
+                }
+            }
+            catch { print($"{name} child [{i}] does not have a trail renderer."); }
+        }
+        activeTrailObject = trailObjects[0];
+        Begin(); // here for debug purposes
+        //print($"Number of trail renderers: {trailRenderers.Count}");
     }
 
     void Update()
@@ -45,9 +66,16 @@ public class EcgAnimation : MonoBehaviour
                 coordInterpolation -= 1f;
             }
 
+            trailRenderers[currentTrailObject].time = Mathf.Lerp(trailTimeResting, trailTimePanic, currentPanic);
             var localPos = GetCurrentPosition();
             activeTrailObject.localPosition = localPos;
             // TODO: set the colour of the line
+            if (prevPanic != currentPanic)
+            {
+                SetTrailColour(trailRenderers[currentTrailObject]);
+            }
+
+            prevPanic = currentPanic;
         }
     }
 
@@ -64,16 +92,50 @@ public class EcgAnimation : MonoBehaviour
         actualHoriz += Mathf.Lerp(horizSpeedResting, horizSpeedPanic, currentPanic) * Time.deltaTime;
         if (actualHoriz >= graphWidth)
         {
+            //print("Switching active trail renderer from: " + currentTrailObject);
             // fade out current trail object, fade in other from x=0. 
-            if (trailRenderers[currentTrailObject] == null)
-                trailRenderers[currentTrailObject] = trailObjects[currentTrailObject].GetComponent<TrailRenderer>();
             trailRenderers[currentTrailObject].emitting = false;
 
-            currentTrailObject = (currentTrailObject + 1) % trailObjects.Length;
-            trailRenderers[currentTrailObject].emitting = true;
+            currentTrailObject = (currentTrailObject + 1) % trailObjects.Count;
+            activeTrailObject = trailObjects[currentTrailObject];
+            UpdateTrailProperties(trailRenderers[currentTrailObject]);
+            
             actualHoriz = 0f;
+            StartCoroutine(ClearTrailNextFrame(currentTrailObject));
         }
 
         return Vector2.right * actualHoriz + Vector2.up * outHeight;
+    }
+
+    public void Begin()
+    {
+        activeTrailObject = trailObjects[currentTrailObject];
+        foreach (var trail in trailRenderers)
+            UpdateTrailProperties(trail);
+    }
+
+    private void UpdateTrailProperties(TrailRenderer trailRenderer)
+    {
+        trailRenderers[currentTrailObject].Clear();
+        trailRenderers[currentTrailObject].emitting = true;
+        SetTrailColour(trailRenderer);
+        trailRenderer.time = Mathf.Lerp(trailTimeResting, trailTimePanic, currentPanic);
+    }
+
+    private void SetTrailColour(TrailRenderer renderer)
+    {
+        Color targetColor = colorGradient.Evaluate(currentPanic);
+        Gradient newGrad = new Gradient();
+        newGrad.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(targetColor, 0.0f), new GradientColorKey(targetColor, 1.0f) },
+            renderer.colorGradient.alphaKeys
+        );
+        renderer.colorGradient = newGrad;
+    }
+
+    private IEnumerator ClearTrailNextFrame(int trailIndex)
+    {
+        yield return new WaitForEndOfFrame();
+        trailRenderers[trailIndex].Clear();
     }
 }
