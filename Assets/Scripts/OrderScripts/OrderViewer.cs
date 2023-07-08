f83a6744a1d0bc3e95f02c8cbe833585d8ac5d47
Kyle [Home PC] <kylethomashammer@gmail.com>
2023-07-08 23:12:42 +1000

    Basic order viewing panel implemented

---
+++ Assets/Scripts/OrderScripts/OrderViewer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using TMPro;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OrderViewer : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh;
+    public Vector2 _tweenPos;
+    public float _tweenTime;
+    public RectTransform rect;
+
+    private Vector2 startPos;
+
+    private bool tweenRunning = false;
+    
+    void Start()
+    {
+        startPos = rect.localPosition;
+        
+        // TODO: Get the string for this
+        textMesh.text = "TEST ORDER HERE";
+    }
+    
+    public void OnHoverEnter()
+    {
+        StartCoroutine(ShowOrder());
+    }
+    
+    public void OnHoverExit()
+    {
+        StartCoroutine(HideOrder());
+    }
+    
+    private IEnumerator ShowOrder()
+    {
+        tweenRunning = true;
+        
+        Tween myTween = rect.DOAnchorPos(_tweenPos, _tweenTime, false).SetEase(Ease.OutCubic);
+        yield return myTween.WaitForCompletion();
+        
+        tweenRunning = false;
+    }
+    
+    private IEnumerator HideOrder()
+    {
+        tweenRunning = true;
+        
+        Vector2 newPosition = new Vector2(startPos.x, startPos.y);
+        
+        Tween myTween = rect.DOAnchorPos(newPosition, _tweenTime, false).SetEase(Ease.OutCubic);
+        yield return myTween.WaitForCompletion();
+        
+        tweenRunning = false;
+    }
+}
---
+++ Assets/Scripts/OrderScripts/OrderViewerHoverTrigger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OrderViewerHoverTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private OrderViewer orderViewer;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        orderViewer.OnHoverEnter();
+        Debug.Log("trigger enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        orderViewer.OnHoverExit();
+        Debug.Log("trigger exit");
+    }
+}
