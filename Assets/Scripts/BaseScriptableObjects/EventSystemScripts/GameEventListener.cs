b082dcf1abc0bbb4291a41acf1acb33c795f953c
Kyle Hammer (Gmail) <kylethomashammer@gmail.com>
2023-07-08 15:29:11 +1000

    Imported SO game events + script can now send strings

---
+++ Assets/Scripts/BaseScriptableObjects/EventSystemScripts/GameEventListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    // Attach this script to an object that wants to listen out for something
+
+    [Tooltip("Event to register with.")]
+    public GameEvent Event;
+
+    [Tooltip("Response to invoke when Event is raised")]
+    public UnityEvent Response;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+    }
+
+    public void TestFunction()
+    {
+        Debug.Log("Test to see if your event is raised");
+    }
+}
