7fb2fa5ef22e1bd0a33fc402007b7f0eb928f4bf
Kyle [Home PC] <kylethomashammer@gmail.com>
2023-07-09 17:00:31 +1000

    Sansirisms is no longer double triggering

--- Assets/Scripts/Sanserisms.cs
+++ Assets/Scripts/Sanserisms.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace DefaultNamespace
@@ -18,34 +19,57 @@ namespace DefaultNamespace
 
         private void OnEnable()
         {
-            inputRemapping.backspaceTyped.AddListener(() =>
-            {
-                Debug.Log("backspace");
-                PlayBlip(uhhBlip, false);
-            });
             inputRemapping.normalLetterTyped.AddListener(() =>
             {
                 Debug.Log("normal");
                 PlayBlip(normalBlip);
             });
-            inputRemapping.doubleLetterTyped.AddListener(() => PlayBlip(stutterBlip));
-            inputRemapping.swappedLetterTyped.AddListener(() => PlayBlip(stutterBlip));
+            
+            inputRemapping.backspaceTyped.AddListener(() =>
+            {
+                Debug.Log("backspace");
+                PlayBlip(uhhBlip, false);
+            });
+            
+            inputRemapping.swappedLetterTyped.AddListener(() =>
+            {
+                Debug.Log("swap");
+                PlayBlip(uhhBlip, false);
+            });
+            
+            inputRemapping.doubleLetterTyped.AddListener(() =>
+            {
+                Debug.Log("double");
+                PlayBlip(uhhBlip, false);
+            });
         }
-
+        
+        private void OnDisable()
+        {
+            inputRemapping.normalLetterTyped.RemoveAllListeners();
+            inputRemapping.backspaceTyped.RemoveAllListeners();
+            inputRemapping.swappedLetterTyped.RemoveAllListeners();
+            inputRemapping.doubleLetterTyped.RemoveAllListeners();
+        }
+        
         private void PlayBlip(AudioClip clip, bool varyPitch = true)
         {
             secondEvent = !secondEvent;
 
             if (secondEvent) return;
             
-            Debug.Log(clip.name);
-            doubleAudioSource.CrossFade(clip, 0.5f, 0.1f);
+            //Debug.Log(clip.name);
+            
+            if (doubleAudioSource != null)
+            {
+                doubleAudioSource.CrossFade(clip, 0.5f, 0.1f);
             
             
-            if (varyPitch)
-                doubleAudioSource.CurrentSource().pitch = 1 + Random.Range(-pitchVariance, pitchVariance);
-            else
-                doubleAudioSource.CurrentSource().pitch = 1;
+                if (varyPitch)
+                    doubleAudioSource.CurrentSource().pitch = 1 + Random.Range(-pitchVariance, pitchVariance);
+                else
+                    doubleAudioSource.CurrentSource().pitch = 1;
+            }
         }
     }
 }
--- Assets/Scripts/TypingScripts/InputRemapping.cs
+++ Assets/Scripts/TypingScripts/InputRemapping.cs
@@ -11,16 +11,18 @@ public class InputRemapping : MonoBehaviour
     [SerializeField]
     public REMAP_TYPE remapType;
     
-    // [SerializeField] private float remapPercentage = 0.05f;
-    
+    // Letter swap params
     // I use strings because it's nicer to store spaces
     private string[] vowels = {"A", "E", "I", "O", "U"};
     private List<int> vowelPositionList = new List<int> { 0, 4, 8, 14, 20 };
     private string[] alphabet = new string[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     private string[] newAlphabetOrder = new string[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private bool letterSwapTriggered = false;
     
+    // Double letter params
     [SerializeField] private bool doubleLettersEnabled = false;
     [SerializeField] private float doubleLettersChance = 0.05f;
+    private bool doubleLetterTriggered = false;
     
     public int numberOfRemaps = 1;
     
@@ -33,7 +35,7 @@ public class InputRemapping : MonoBehaviour
     private string lastTypedCharacter;
     private int lastTypedCharacterPosition = 0;
     
-    private bool isProgramChangingText;
+    private bool isProgramChangingText = false;
 
     [NonSerialized] public UnityEvent normalLetterTyped = new();
     [NonSerialized] public UnityEvent swappedLetterTyped = new();
@@ -145,7 +147,13 @@ public class InputRemapping : MonoBehaviour
     
     public void OnLetterTyped()
     {
-        inputField.text = inputField.text.ToUpper();
+        // Convert to uppercase AND RETURN TO NOT RUN THE REST
+        // Updating the inputField to uppercase will run this function again anyway
+        if (inputField.text != inputField.text.ToUpper())
+        {
+            inputField.text = inputField.text.ToUpper();
+            return;
+        }
         
         currentText = inputField.text;
         DaddyManager.instance.OnInput();
@@ -153,6 +161,7 @@ public class InputRemapping : MonoBehaviour
         // If the change is made by the program, don't do anything
         if (isProgramChangingText)
         {
+            isProgramChangingText = false;
             return;
         }
         
@@ -165,7 +174,32 @@ public class InputRemapping : MonoBehaviour
         GetLastTypedCharacter();
         ChangeLetterIfApplicable();
         RandomiseDoubleLetterChance();
+
+        PlaySoundEvent();
+        
         previousText = currentText;
+    }
+
+    private void PlaySoundEvent()
+    {
+        if (doubleLetterTriggered)
+        {
+            doubleLetterTyped.Invoke();
+            doubleLetterTriggered = false;
+        }
+        else if(letterSwapTriggered)
+        {
+            swappedLetterTyped.Invoke();
+            letterSwapTriggered = false;
+        }
+        else if (currentText.Length <= previousText.Length)
+        {
+            backspaceTyped.Invoke();
+        }
+        else if (currentText.Length > previousText.Length)
+        {
+            normalLetterTyped.Invoke();
+        }
     }
 
     private void GetLastTypedCharacter()
@@ -180,15 +214,13 @@ public class InputRemapping : MonoBehaviour
                     lastTypedCharacter = currentText[i].ToString();
                     lastTypedCharacterPosition = i;
                     
-                    if(!isProgramChangingText)
-                        normalLetterTyped.Invoke();
-                    
                     break;
                 }
             }
         }
         // Edge case check for if words are highlighted and replaced with a single letter
-        else if (currentText.Length < previousText.Length && previousText.Length - currentText.Length >= 2 && currentText != "")
+        else if (currentText.Length < previousText.Length && previousText.Length - currentText.Length >= 2 &&
+                 currentText != "")
         {
             for (int i = 0; i < previousText.Length; i++)
             {
@@ -201,11 +233,6 @@ public class InputRemapping : MonoBehaviour
                     break;
                 }
             }
-        }
-        else if (currentText.Length < previousText.Length)
-        {
-            if(!isProgramChangingText)
-                backspaceTyped.Invoke();
         }
         else
         {
@@ -239,7 +266,8 @@ public class InputRemapping : MonoBehaviour
         // if (previousText.Length > currentText.Length)
         //     return;
         
-        ReplaceLastTypedCharacter();
+        if(GetNewCharacter() != lastTypedCharacter)
+            ReplaceLastTypedCharacter();
     }
     
     private void RandomiseDoubleLetterChance()
@@ -287,11 +315,11 @@ public class InputRemapping : MonoBehaviour
             GetLastTypedCharacter();
             ChangeLetterIfApplicable();
             
-            isProgramChangingText = false;
+            doubleLetterTriggered = true;
+            
+            //isProgramChangingText = false;
             
             //Debug.Log("Double letter");
-            
-            doubleLetterTyped.Invoke();
         }
     }
     
@@ -309,9 +337,9 @@ public class InputRemapping : MonoBehaviour
         
         inputField.text = newText;
         
-        swappedLetterTyped.Invoke();
+        letterSwapTriggered = true;
         
-        isProgramChangingText = false;
+        //isProgramChangingText = false;
     }
 
     private string GetNewCharacter()
