b714636659953e122b9b552ed839007f2d4e2a9e
VoidAwake <voidawake90@gmail.com>
2023-07-09 12:31:08 +1000

    Set up main menu button functionality

---
+++ Assets/Scripts/Menu/SceneChangeButton.cs
@@ -0,0 +1,14 @@
+﻿using Eflatun.SceneReference;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SceneChangeButton : MonoBehaviour
+{
+    [SerializeField] private SceneReference sceneReference;
+    private void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(sceneReference.BuildIndex));
+    }
+}
