339ca1f95843abaa4c6d35efc48e509065249365
VoidAwake <voidawake90@gmail.com>
2023-07-09 17:05:00 +1000

    Credits animation

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] private RectTransform credits;
+    [SerializeField] private CanvasGroup mainMenu;
+    [SerializeField] private Vector2 shownPos;
+    [SerializeField] private float tweenTime;
+
+    private Vector2 hiddenPos;
+    
+    private void Awake()
+    {
+        hiddenPos = credits.anchoredPosition;
+    }
+
+    public void ShowCredits()
+    {
+        mainMenu.DOFade(0, tweenTime).SetEase(Ease.OutCubic);
+        credits.DOAnchorPos(shownPos, tweenTime, false).SetEase(Ease.OutCubic);
+    }
+
+    public void HideCredits()
+    {
+        mainMenu.DOFade(1, tweenTime).SetEase(Ease.InCubic);
+        credits.DOAnchorPos(hiddenPos, tweenTime, false).SetEase(Ease.InCubic);
+    }
+}
