3e46f33353e6af28af8316e4e0be9f3db6c540b6
VoidAwake <voidawake90@gmail.com>
2023-07-22 16:47:54 +1000

    Clean up CONTWEEN

--- Assets/CONTWEEN.cs
+++ Assets/CONTWEEN.cs
@@ -1,24 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
 public class CONTWEEN : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] private RectTransform pos;
-    void Start()
+    
+    private void Start()
     {
-        if (pos)
-        {
-            RectTransform rect = GetComponent<RectTransform>();
-            rect.DOAnchorPos(pos.anchoredPosition, 0.5f, false).SetEase(Ease.OutCubic);
-        }
-    }
-
-    // Update is called once per frame
-    IEnumerator tween()
-    {
-        yield return null;
+        if (!pos) return;
+        
+        RectTransform rect = GetComponent<RectTransform>();
+        
+        rect.DOAnchorPos(pos.anchoredPosition, 0.5f).SetEase(Ease.OutCubic);
     }
 }
